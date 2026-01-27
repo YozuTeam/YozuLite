273ff8a4a6db912ec1d0a07b6b6fb8e7b275238e
Victor Agahi <victor.a@padoa-group.com>
2026-01-27 13:50:25 +0100

    shared libs

--- yozu-lite/next.config.ts
+++ yozu-lite/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: [
+    "@yozu/shared",
+    "class-validator",
+    "class-transformer",
+    "@nestjs/mapped-types",
+  ],
 };
 
 export default nextConfig;
